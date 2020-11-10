d36893677b3eda23bc86ac1fc8aaa6c595d67f51
KristjanLeo <krissi.leo@gmail.com>
2020-11-10 14:20:53 +0000

    Map

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { el, element, formatDate } from './lib/utils';
 import { fetchEarthquakes } from './lib/earthquakes.js';
+import { init } from './lib/map.js';
 // importa öðru sem þarf...
 
 document.addEventListener('DOMContentLoaded', async () => {	
 	await fetchEarthquakes().then((data) => {
 		console.log(data); /* Test */
 	});
-
+	const theMap = document.querySelector('.map');
+	init(theMap);
 });
--- src/lib/map.js
+++ src/lib/map.js
@@ -10,7 +10,7 @@ export function createPopup(geojson, content) {
 // Býr til Leaflet kort og setur miðju á (0, 0) í zoom level 2
 export function init(el) {
   // TODO
-
+  const map = L.map(el).setView([0, 0], 2);
   // Bætum við "tiles" frá OSM sem eru open source. Gætum líka
   // notað frá Google, mapbox eða fleirum en þyrftum þá aðgang
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
